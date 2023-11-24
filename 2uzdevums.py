fd292a0356a8ae531922a82ae93d94dd1d707990
RihardsLiepins <rihards.liepins@grobinasskola.lv>
2023-11-24 09:55:19 +0200

    2.uzdevums

---
+++ 2uzdevums.py
@@ -0,0 +1,9 @@
+# Definēju sākuma skaitli
+skaitlis = 1
+
+# Izveidoju while ciklu kura kvadrāts nav lielāks par 1000
+while skaitlis**2 <= 1000:
+    skaitlis += 1
+
+# Izveido rezultātu
+print("Pirmais skaitlis kura kvadrāts nav lielāks par 1000, ir:", skaitlis)
